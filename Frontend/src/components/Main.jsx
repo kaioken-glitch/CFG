536fc190e63b17ef4daed39ce04f6e14ff1d8c24
kaioken-glitch <lightningcyranus@gmail.com>
2025-07-22 17:35:35 +0200

    added the dashboard component

--- Frontend/src/components/Main.jsx
+++ Frontend/src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Main({ children }) {
     return (
-        <div className="main">
+        <div className="main w-full min-h-screen overflow-y-auto">
             {children}
         </div>
     )
